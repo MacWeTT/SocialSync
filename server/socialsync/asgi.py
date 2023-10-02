f628ce9a193d2a28bd8c1ced6ed5db39b8976052
Manas Bajpai <97174936+MacWeTT@users.noreply.github.com>
2023-10-02 12:01:26 +0530

    upgraded server to asgi

--- server/socialsync/asgi.py
+++ server/socialsync/asgi.py
@@ -1,16 +1,14 @@
-"""
-ASGI config for socialsync project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 import os
-
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socialsync.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "socialsync.settings")
 
-application = get_asgi_application()
+django_asgi = get_asgi_application()
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi,
+        # Just HTTP for now. (We can add other protocols later.)
+    }
+)
